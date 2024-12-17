57f140f5145712658af20c84f2ccf5d32376c04d
Yuusuke Ishii <111514756+Sanchi7412@users.noreply.github.com>
2024-12-17 20:25:23 +0900

    HTTP GET v0
    開発用初期版

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,19 @@ const SS_NAME: string = "scraping_data";
 
 const db = SpreadSheetsSQL.open(DB_ID, SS_NAME);
 
-function main() {
-  console.log("Hello, World!");
-  let test_data = getDB();
-}
+interface get
 
 const doGet = (e) => {
-  const path: string = e?.parameter.path;
+  const path: string = e?.parameter.page;
 
   let data: string[] = getDB();
-  return ContentService.createTextOutput(JSON.stringify(data));
+
+  return ContentService.createTextOutput(JSON.stringify({"data": data})).setMimeType(ContentService.MimeType.JSON);
 };
 
 function getDB() {
   let data: string[] = db
-    .select(["paper_id", "title", "abstract", "category_list", "upload_date"])
+    .select(["paper_id", "title", "abstract", "category_list", "url", "upload_date"])
     .result();
 
   return data;
